507f3926cbe5d745b4726d8af3db2907996b1249
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-11-21 09:39:49 +0530

    Update UnIvErZe.py

--- UnIvErZe.py
+++ UnIvErZe.py
@@ -3131,7 +3131,7 @@ def main():
     )
 
     
-    set_app_background("Gemini_Generated_Image_iyeooniyeooniyeo.jpg") 
+    set_app_background("Gemini_Generated_Image_iyeooniyeoniyeo.jpg") 
     
     # --- CUSTOM CSS: TRANSPARENT SIDEBAR & HEADER ---
     st.markdown("""
